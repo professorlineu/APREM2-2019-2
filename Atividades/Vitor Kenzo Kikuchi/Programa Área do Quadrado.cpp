c41c4b0339ba8dde7348f81a351fe6766350fefa
Vitor Kenzo Kikuchi <53536885+vitork3nzo@users.noreply.github.com>
2019-08-07 16:07:06 -0300

    Prog Área do Quadrado

---
+++ Atividades/Vitor Kenzo Kikuchi/Programa Área do Quadrado.cpp
@@ -0,0 +1,29 @@
+/**********************************************************
+- Autor:     Seu nome
+- Descrição: Breve descrição do programa
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+int iLado = 0;
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+cout << "Digite a medida do lado do quadrado: ";
+cin >> iLado;
+
+cout << "A área do quadrado é igual a "
+     << iLado * iLado
+     << endl;
+
+    return 0;
+}

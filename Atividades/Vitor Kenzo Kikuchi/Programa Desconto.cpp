bb571cb60d64a3334580acf94db744fb749b6471
Vitor Kenzo Kikuchi <53536885+vitork3nzo@users.noreply.github.com>
2019-08-07 16:05:31 -0300

    Programa de Desconto

---
+++ Atividades/Vitor Kenzo Kikuchi/Programa Desconto.cpp
@@ -0,0 +1,35 @@
+/**********************************************************
+- Autor:     Seu nome
+- Descrição: Breve descrição do programa
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+float fPreco = 0;
+float fDesconto = 0;
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+cout << "Digite o preço do produto: ";
+cin >> fPreco;
+
+cout << "Digite o desconto: ";
+cin >> fDesconto;
+
+cout << "O valor do produto com desconto é de "
+     << fPreco - fDesconto
+     << " reais."
+     << endl;
+
+
+    return 0;
+}

204d7e03c06a34e958d339d08279ea3db2433106
Vitor Kenzo Kikuchi <53536885+vitork3nzo@users.noreply.github.com>
2019-08-14 11:05:26 -0300

    Programa km carro

---
+++ Atividades/Vitor Kenzo Kikuchi/Eficiencia carro.cpp
@@ -0,0 +1,37 @@
+/**********************************************************
+- Autor:     Seu nome
+- Descrição: Breve descrição do programa
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+float fLitros = 0;
+
+float fEficiencia = 0;
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+cout << "Quantos litros de combústivel estão no tanque do seu carro? ";
+cin >> fLitros;
+
+cout << "Quantos km seu carro percorre com 1 litro? ";
+cin >> fEficiencia;
+
+cout << "Você pode percorrer "
+     << fLitros * fEficiencia
+     << " km."
+     << endl;
+
+
+    return 0;
+
+}

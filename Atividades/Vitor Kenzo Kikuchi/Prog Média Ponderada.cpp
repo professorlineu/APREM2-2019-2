84133635d54a2401d4ee8029c68d26347bb727e3
Vitor Kenzo Kikuchi <53536885+vitork3nzo@users.noreply.github.com>
2019-08-13 21:28:10 -0300

    Programa de Média Ponderada

---
+++ Atividades/Vitor Kenzo Kikuchi/Prog Média Ponderada.cpp
@@ -0,0 +1,35 @@
+/**********************************************************
+- Autor:     Seu nome
+- Descrição: Breve descrição do programa
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+float fNota1 = 0;
+
+float fNota2 = 0;
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+cout << "Quanto você tirou na primeira prova? ";
+cin >> fNota1;
+
+cout << "Quanto você tirou na segunda prova? ";
+cin >> fNota2;
+
+cout << "Considerando que a primeira prova tem peso 2 e a segunda 3, sua média ponderada é de "
+     << (fNota1*2 + fNota2*3)/5
+     << " pontos."
+     << endl;
+
+    return 0;
+}

f0678a83f323aecf266df172f151f967337ffe14
RarianSetoguchi <53540013+RarianSetoguchi@users.noreply.github.com>
2019-08-14 17:31:42 -0300

    media ponderada de nota

---
+++ Atividades/Rarian/media.cpp
@@ -0,0 +1,36 @@
+/**********************************************************
+- Autor:     Rarian
+- Descrição: Exercício 4
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+    float fN1 = 0;
+    float fN2 = 0;
+    int iP1 = 2;
+    int iP2 = 3;
+    int iDiv = 5;
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+    cout << "Digite a primeira nota de peso 2: ";
+    cin >> fN1;
+    cout << "Digite a segunda nota de peso 3: ";
+    cin >> fN2;
+
+    cout << "A média ponderada das notas é de: "
+         << ((fN1 * iP1) + (fN2 * iP2)) / iDiv
+         << endl;
+
+
+    return 0;
+}

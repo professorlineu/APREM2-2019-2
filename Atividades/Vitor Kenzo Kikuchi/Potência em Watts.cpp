3dcd44f0ad175e503464d62222752f6aabc719fd
Vitor Kenzo Kikuchi <53536885+vitork3nzo@users.noreply.github.com>
2019-08-13 22:44:04 -0300

    Programa Potência no cômodo

---
+++ Atividades/Vitor Kenzo Kikuchi/Potência em Watts.cpp
@@ -0,0 +1,35 @@
+/**********************************************************
+- Autor:     Seu nome
+- Descrição: Breve descrição do programa
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+
+    //Declaração de variáveis
+float fLado1= 0;
+
+float fLado2 = 0;
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+cout << "Para saber a potência em Watts necessária no seu cômodo, digite a medida em metros de um dos lados: ";
+cin >> fLado1;
+
+cout << "Digite a medida do outro lado: ";
+cin >> fLado2;
+
+cout << "A potência necessária no seu cômodo é de "
+     << (fLado1*fLado2)*18
+     << "W."
+     <<endl;
+    return 0;
+}

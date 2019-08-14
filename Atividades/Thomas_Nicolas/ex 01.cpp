c0df1ee6a254a6e886fe372f03e7f325ca8f47bb
ThomasTaquetto <53537358+ThomasTaquetto@users.noreply.github.com>
2019-08-14 17:59:50 -0300

    upload

---
+++ Atividades/Thomas_Nicolas/ex 01.cpp
@@ -0,0 +1,33 @@
+/**********************************************************
+- Autor:     thomas nicolas
+- Descrição: ex 1
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+
+    int ivalor1 = 0;
+    int ivalor2 = 0;
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+
+    cout   << "insira o primeiro valor: ";
+    cin >> ivalor1;
+
+    cout   << "insira o segundo valor: ";
+    cin >> ivalor2;
+
+    cout   <<"o valor da subtração é: " <<ivalor1-ivalor2;
+
+    return 0;
+}

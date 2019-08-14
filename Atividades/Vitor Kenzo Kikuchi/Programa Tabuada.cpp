9d113b7010775e87f363db80d0012ddd715e483a
Vitor Kenzo Kikuchi <53536885+vitork3nzo@users.noreply.github.com>
2019-08-13 22:00:01 -0300

    Progama Tabuada

---
+++ Atividades/Vitor Kenzo Kikuchi/Programa Tabuada.cpp
@@ -0,0 +1,76 @@
+/**********************************************************
+- Autor:     Seu nome
+- Descrição: Breve descrição do programa
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+int inumber = 0;
+
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+cout << "Digite o número que você deseja saber a tabuada: ";
+cin >> inumber;
+
+cout << inumber
+     << "x1="
+     << inumber*1
+     << endl;
+
+cout << inumber
+     << "x2="
+     << inumber*2
+     << endl;
+
+cout << inumber
+     << "x3="
+     << inumber*3
+     << endl;
+
+cout << inumber
+     << "x4="
+     << inumber*4
+     << endl;
+
+cout << inumber
+     << "x5="
+     << inumber*5
+     << endl;
+
+cout << inumber
+     << "x6="
+     << inumber*6
+     << endl;
+
+cout << inumber
+     << "x7="
+     << inumber*7
+     << endl;
+
+cout << inumber
+     << "x8="
+     << inumber*8
+     << endl;
+
+cout << inumber
+     << "x9="
+     << inumber*9
+     << endl;
+
+cout << inumber
+     << "x10="
+     << inumber*10
+     << endl;
+
+    return 0;
+}

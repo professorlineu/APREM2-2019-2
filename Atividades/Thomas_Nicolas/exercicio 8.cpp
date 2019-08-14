c9ce0e57a8ab225c5cc608b86d29b48b1ba49c7a
ThomasTaquetto <53537358+ThomasTaquetto@users.noreply.github.com>
2019-08-14 16:02:25 -0300

    upload

---
+++ Atividades/Thomas_Nicolas/exercicio 8.cpp
@@ -0,0 +1,28 @@
+/**********************************************************
+- Autor:     Thomas Nicolas
+- Descrição:
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+float fpeso = 0;
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+
+    cout    << "insira seu peso em kilos: ";
+    cin     >> fpeso;
+
+    cout    << "seu peso em gramas: " <<fpeso*1000 <<"g";
+
+    return 0;
+}

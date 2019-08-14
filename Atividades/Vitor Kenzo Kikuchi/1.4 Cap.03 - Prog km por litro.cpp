ee4a20d1e21dd1940a9de9d02623a10740a2249e
Vitor Kenzo Kikuchi <53536885+vitork3nzo@users.noreply.github.com>
2019-08-14 15:37:53 -0300

    delete

--- Atividades/Vitor Kenzo Kikuchi/1.4 Cap.03 - Prog km por litro.cpp
+++ Atividades/Vitor Kenzo Kikuchi/1.4 Cap.03 - Prog km por litro.cpp
@@ -1,6 +1,6 @@
 /**********************************************************
-- Autor:     Seu nome
-- Descrição: Breve descrição do programa
+- Autor:     Vitor Kenzo Kikuchi
+- Descrição: Exercício 
 **********************************************************/
 #include <iostream>
 #include <locale.h>

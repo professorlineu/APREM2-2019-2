d57a8ceadd4d013ee767128aa47353930a87fd19
Milene-Dias <53537044+Milene-Dias@users.noreply.github.com>
2019-08-15 14:03:26 -0300

    Create hip.cpp

---
+++ Milene Dias/hip.cpp
@@ -0,0 +1,31 @@
+/**********************************************************
+- Autor:     Milene Caroline B. D. Chaves
+- Descrição: hipotenusa
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    int icateto1=0;
+    int icateto2=0;
+    int ihipotenusa=0;
+
+    cout<<" insira o valor do primeiro cateto ";
+    cin>>icateto1;
+
+    cout<<" insira on valor do segundo cateto ";
+    cin>>icateto2;
+
+    ihipotenusa*ihipotenusa= icateto1*icateto1+ icateto2*icateto1;
+
+    cout<< "a hipotenusa vale: ";
+    cout ihipotenusa;
+
+
+
+
+}

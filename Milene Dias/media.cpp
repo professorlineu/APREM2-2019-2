8e2b61ffb260e34ef3912c6956ab8a5dfeb63520
Milene-Dias <53537044+Milene-Dias@users.noreply.github.com>
2019-08-14 20:00:40 -0600

    Create media.cpp

---
+++ Milene Dias/media.cpp
@@ -0,0 +1,35 @@
+/**********************************************************
+- Autor:     Milene Caroline B. D. Chaves
+- Descrição: soma e divide
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+      int isom=0
+      int idivide=0
+      int inumero1=0;
+      int inumero2=0;
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color B5");
+
+    cout<< " numero 1:" ;
+    cin>> inumero1;
+    cout<< " numero 2:" ;
+    cin>> inumero2;
+
+    isoma=inumero1+inumero2;
+    idivide=inumero1/inumero2;
+    
+    cout << "o numero encontrado é: " << isub << endl;
+
+
+    return 0;
+}

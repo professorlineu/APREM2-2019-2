03ed8ba10305d2e96177f7a5d09328ffe29b418f
naelgouveaa <53536992+naelgouveaa@users.noreply.github.com>
2019-08-15 13:27:31 -0300

    salário depois das 2 contas

---
+++ Vanderlei/Atividade7.cpp
@@ -0,0 +1,42 @@
+/**********************************************************
+- Autor:     Vandelei Natanael
+- Descrição: Celsius para Fahrenheit
+**********************************************************/
+
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+    float isalario = 0;
+    float iconta1 = 0;
+    float iconta2 = 0;
+    float ifinal = 0;
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    cout << "O salário que João recebeu foi de: ";
+    cin >> isalario;
+
+    cout << "O valor da conta 1 é: ";
+    cin >> iconta1;
+
+    cout << "O valor da conta 2 é: ";
+    cin >> iconta2;
+
+    ifinal = isalario - (iconta1+iconta1*0.02 + iconta2+iconta2*0.02);
+
+    cout << "O salário final de João é de " << ifinal << " reais";
+    //Código do programa
+
+
+
+    return 0;
+
+}

76b30be08b549b30cae3ab916b1d462ba7862b7e
Gabriel Oliveira <53537229+outrogabriel2@users.noreply.github.com>
2019-08-14 23:57:36 -0300

    Add files via upload

---
+++ divisao.cpp
@@ -0,0 +1,32 @@
+/**********************************************************
+
+- Autor: Gabriel Guimarães
+
+
+- Descrição: Lista 1 - Exercício 3
+
+**********************************************************/
+
+
+
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+
+{
+    int in1 = 0;
+    int in2 = 0;
+    int idivisao = 0;
+
+    cout << "insira 2 numeros";
+    cin >> in1;
+    cin >> in2;
+    idivisao = in1/in2;
+
+    cout << idivisao;
+
+  }
---
+++ media.cpp
@@ -0,0 +1,41 @@
+/**********************************************************
+
+- Autor: Gabriel Guimarães
+
+
+- Descrição: Lista 1 - Exercício 4
+
+**********************************************************/
+
+
+
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+
+{
+    int in1 = 0;
+    int in2 = 0;
+    int imediaf = 0;
+
+    cout << "nota de peso 2:    ";
+    cin >> in1;
+    cout << "nota de peso 3:    ";
+    cin >> in2;
+
+    imediaf = (in1 + in2)/5;
+
+    cout << "sua nota e:    ";
+    cout << imediaf;
+
+
+
+
+
+
+
+}

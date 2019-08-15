f52c0630b4c8af79ba18fcf5dcfc141cfa49199d
Joaovitormorais <53537236+Joaovitormorais@users.noreply.github.com>
2019-08-15 00:44:27 -0300

    Programa de desconto

---
+++ Atividades/João Vitor Morais/ex05.cpp
@@ -0,0 +1,34 @@
+/**********************************************************
+- Autor:     João vitor morais
+- Descrição: Faça um programa que receba o preço de um produto, calcule e mostre o novo preço, sabendo-se
+que este sofreu um desconto de 10%.
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+float fpreco = 0;
+float fdesc = 0;
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+cout << "apresente o preço do produto: ";
+cin  >> fpreco;
+
+cout << "apresente o desconto feito: ";
+cin  >> fdesc;
+
+cout << "o preço do produto com o desconto é:  "
+     << fpreco - fdesc
+     << endl;
+
+    return 0;
+}

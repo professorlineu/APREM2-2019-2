8c29c8f724da78f7e41531568395e739d12aab49
Milene-Dias <53537044+Milene-Dias@users.noreply.github.com>
2019-08-15 13:55:03 -0300

    Create salario.cpp

---
+++ Milene Dias/salario.cpp
@@ -0,0 +1,24 @@
+/**********************************************************
+- Autor:     Milene Caroline B. D. Chaves
+- Descrição: salario
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+float fsalario=0;
+float fresultado=0;
+
+cout<<"insira seu salario:";
+cin>>fsalario;
+
+fresultado=(fsalario/998);
+
+cout<<" seu salario vale " <<fresultado <<" salario minimos";
+
+
+}

e4b58a5b014c72afe063b11837e167461c257bd0
Vitor Kenzo Kikuchi <53536885+vitork3nzo@users.noreply.github.com>
2019-08-15 13:32:34 -0300

    EX 01 - Programa Nota Final

---
+++ Atividades/Vitor Kenzo Kikuchi/2.1 Cap.04 - Prog11.cpp
@@ -0,0 +1,65 @@
+/**********************************************************
+- Autor:     Seu nome
+- Descrição: Breve descrição do programa
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+float fNota1 = 0;
+
+float fNota2 = 0;
+
+float fNota3 = 0;
+
+float fNota4 = 0;
+
+float fNotaFinal = 0;
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+
+cout << "Quanto você tirou na primeira prova? ";
+cin  >> fNota1;
+
+cout << "Segunda prova? ";
+cin  >> fNota2;
+
+cout << "Terceira prova? ";
+cin  >> fNota3;
+
+cout << "E na última? ";
+cin  >> fNota4;
+
+fNotaFinal = (fNota1 + fNota2 + fNota3 + fNota4)/4;
+
+cout << "A sua média aritmética final é "
+     << fNotaFinal
+     << endl;
+
+if (fNotaFinal > 7)
+{
+    cout << "Você está aprovado!"
+         << endl;
+}
+
+else if (fNotaFinal < 3)
+{
+    cout << "Quem é capaz de tirar esse tipo de nota? "
+         << endl;
+}
+else
+{
+    cout << "Você está reprovado. Ou vai pra recuperação, eu não sei, sou só um programa."
+         << endl;
+}
+    return 0;
+}

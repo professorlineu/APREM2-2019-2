9742f3d4a92ac80d3878486c81038e959a0c41e8
ramonmacedo98 <53537120+ramonmacedo98@users.noreply.github.com>
2019-08-15 22:44:05 -0300

    ex 13 tabuada

---
+++ Atividades/Ramon/ex 13 tabuada.cpp
@@ -0,0 +1,82 @@
+;/**********************************************************
+- Autor:     ramon macedo
+- Descrição:. Faça um programa que calcule e mostre a tabuada de um número digitado pelo usuário.
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+float  fnumtab = 0;
+
+    //Código do programa
+
+cout  << "mostrador de tabuada. "
+      << endl;
+
+cout  << "digite o numero que quer saber a tabuada: ";
+cin   >> fnumtab;
+
+cout  << fnumtab
+      << " x 0 = "
+      << fnumtab*0
+      << endl;
+
+cout  << fnumtab
+      << " x 1 = "
+      << fnumtab*1
+      << endl;
+
+cout  << fnumtab
+      << " x 2 = "
+      << fnumtab*2
+      << endl;
+
+cout  << fnumtab
+      << " x 3 = "
+      << fnumtab*3
+      << endl;
+
+cout  << fnumtab
+      << " x 4 = "
+      << fnumtab*4
+      << endl;
+
+cout  << fnumtab
+      << " x 5 = "
+      << fnumtab*5
+      << endl;
+
+cout  << fnumtab
+      << " x 6 = "
+      << fnumtab*6
+      << endl;
+
+cout  << fnumtab
+      << " x 7 = "
+      << fnumtab*7
+      << endl;
+
+cout  << fnumtab
+      << " x 8 = "
+      << fnumtab*8
+      << endl;
+
+cout  << fnumtab
+      << " x 9 = "
+      << fnumtab*9
+      << endl;
+
+cout  << fnumtab
+      << " x 10 = "
+      << fnumtab*10
+      << endl;
+
+
+
+      return 0;
+}

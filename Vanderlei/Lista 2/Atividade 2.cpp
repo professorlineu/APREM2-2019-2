13fca3f00e300ee703e878b42a340a57bf51947d
Vanderlei Natanael <53536992+naelgouveaa@users.noreply.github.com>
2019-08-21 17:22:22 -0300

    Exame

---
+++ Vanderlei/Lista 2/Atividade 2.cpp
@@ -0,0 +1,47 @@
+/**********************************************************
+- Autor:   vanderlei natanael
+- Descrição: Média aluno com exame
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+
+    float iNo1 = 0;
+    float iNo2 = 0;
+    float imedia = 0;
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+
+    cout << "digite o nota 1: ";
+    cin >> iNo1;
+
+    cout << "digite o nota 2: ";
+    cin >> iNo2;
+
+    imedia = (iNo1+iNo2)/2;
+
+    cout << "a sua nota foi de " << imedia;
+
+    if (imedia>=7)
+      cout<< ", parabéns você foi aprovado! :)";
+
+    if (7>imedia && imedia>=3)
+
+        cout<< ", você foi para exame, estude bem.";
+
+    if (imedia<3)
+        cout << ", você foi reprovado, se fudeu! :D";
+
+
+    return 0;
+}

8fe92574a4c2c3ee71cc8648c0f207f51e76500e
Vanderlei Natanael <53536992+naelgouveaa@users.noreply.github.com>
2019-08-21 17:31:02 -0300

    Senha

---
+++ Vanderlei/Lista 2/Atividade 3.cpp
@@ -0,0 +1,28 @@
+//**********************************************************
+//- Autor: Vanderlei Natanael
+//- Descrição: Senha
+//**********************************************************/
+
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    float inumero1 = 0;
+
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    cout << "digite a senha: ";
+    cin  >> inumero1;
+
+    if (inumero1==4531)
+    
+       cout << "Acesso permitido!";
+       
+    else
+        cout << " Acesso negado, tente novamente!";
+}

68974b1b12a3ff98728d766eedc239ace9c6f9e0
Vitor Kenzo Kikuchi <53536885+vitork3nzo@users.noreply.github.com>
2019-08-21 12:29:44 -0300

    EX 17 - Programa Senha

---
+++ Atividades/Vitor Kenzo Kikuchi/2.9 Cap.04 - Prog18.cpp
@@ -0,0 +1,52 @@
+/**********************************************************
+- Autor:     Seu nome
+- Descrição: Breve descrição do programa
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+float fSenha = 0;
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color 8F");
+
+    //Código do programa
+
+cout << "\n\n"
+     << "              ___________\n"
+     << "              |---------|\n"
+     << "              | 1  2  3 |\n"
+     << "              | 4  5  6 |\n"
+     << "              | 7  8  9 |\n"
+     << "              |    0    |\n"
+     << "              |---------|\n\n"
+     << endl;
+
+cout << "    Digite a senha de quatro digitos:\n\n";
+cout << "                 ";
+cin  >> fSenha;
+
+if (fSenha == 6753)
+
+{
+cout << "\n            Acesso Liberado!"
+     << "\n\n        (Para absolutamente nada)\n\n\n\n"
+     << endl;
+}
+else
+{
+cout << "\n             Acesso Negado.\n\n\n\n"
+     << endl;
+}
+
+
+    return 0;
+}

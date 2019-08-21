c3ce6c255e7a8bfbeefb2e158c5aaf8120bac171
RarianSetoguchi <53540013+RarianSetoguchi@users.noreply.github.com>
2019-08-21 11:55:39 -0300

    Calcula a área de um trapézio

---
+++ Atividades/Rarian/trapezio(2).cpp
@@ -0,0 +1,41 @@
+/**********************************************************
+- Autor:     Rarian
+- Descrição: Produto com desconto
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+
+    float fAltura = 0;
+    float fBase1 = 0;
+    float fBase2 = 0;
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+
+    cout << "Programa para calcular a área de um trapézio.\n";
+
+    cout << "Digite a altura do trapézio:";
+    cin >> fAltura;
+
+    cout << "Digite o comrpimento da base maior: ";
+    cin >> fBase1;
+
+    cout << "Digite o comprimento da base menor: ";
+    cin >> fBase2;
+
+    float fArea = (fBase1+fBase2)*fAltura/2;
+
+    cout << "A área do trapézio é de " << fArea << ".";
+
+    return 0;
+}

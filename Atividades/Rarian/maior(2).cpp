3ba874a85acda1b3222270413f7f25c521048f84
RarianSetoguchi <53540013+RarianSetoguchi@users.noreply.github.com>
2019-08-21 17:42:43 -0300

    Lista 2 ex 4

---
+++ Atividades/Rarian/maior(2).cpp
@@ -0,0 +1,48 @@
+/**********************************************************
+- Autor:Augusto Rarian
+- Descriчуo: lista 2 ex 4
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+int inumero1=0;
+int inumero2=0;
+int inumero3=0;
+
+cout<<" digite o primeiro numero";
+cin>> inumero1;
+
+cout<<" digite o segundo numero";
+
+cin>> inumero2;
+
+cout<<" digite o terceiro numero";
+cin>> inumero3;
+
+
+
+if   (inumero1>inumero2 && inumero1>inumero3)
+    {
+        cout<< "O maior numero é" << inumero1
+         << endl;
+    }
+
+ if  (inumero3>inumero1 && inumero2<inumero3)
+    {
+        cout<< "O maior numero é" << inumero3
+         << endl;
+    }
+
+if (inumero2>inumero1 && inumero3<inumero2)
+    {
+        cout<< "O maior numero é" << inumero2
+         << endl;
+    }
+
+
+}

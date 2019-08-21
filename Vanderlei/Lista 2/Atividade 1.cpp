5bb2cef01c9d70b24583c43f42761aed944155ee
Vanderlei Natanael <53536992+naelgouveaa@users.noreply.github.com>
2019-08-21 17:08:29 -0300

    Média Aluno

---
+++ Vanderlei/Lista 2/Atividade 1.cpp
@@ -0,0 +1,51 @@
+/**********************************************************
+- Autor:   vanderlei natanael
+- Descrição: Média aluno
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+
+    float iNo1 = 0;
+    float iNo2 = 0;
+    float iNo3 = 0;
+    float iNo4 = 0;
+    float imedia = 0;
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+
+    cout << "digite o nota 1: ";
+    cin >> iNo1;
+
+    cout << "digite o nota 2: ";
+    cin >> iNo2;
+
+    cout << "digite o nota 3: ";
+    cin >> iNo3;
+
+    cout << "digite o nota 4: ";
+    cin >> iNo4;
+
+    imedia = (iNo1+iNo2+iNo3+iNo4)/4;
+
+    cout << "a sua nota foi de " << imedia;
+
+    if (imedia>=7)
+      cout<< ", parabéns você foi aprovado! :)";
+
+    else
+        cout << ", você foi reprovado, se fudeu! :D";
+
+
+    return 0;
+}

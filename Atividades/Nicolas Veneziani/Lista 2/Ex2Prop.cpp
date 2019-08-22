ff526ffcfbdf6bace7c5cd670e0ee6f921ec4947
nicolasveneziani <53539983+nicolasveneziani@users.noreply.github.com>
2019-08-22 00:00:05 -0300

    Atividade em casa.

---
+++ Atividades/Nicolas Veneziani/Lista 2/Ex2Prop.cpp
@@ -0,0 +1,54 @@
+/**********************************************************
+- Autor:     Nicolas Veneziani
+- Descrição: Exercício 1 dos propostos (Lista 2).
+**********************************************************/
+ #include <iostream>
+ #include <locale.h>
+ #include <cstdlib>
+ #include <cmath>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+    float fNotaP1 = 0;
+    float fNotaP2 = 0;
+    float fMed = 0;
+
+
+
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+    cout << "Digite a nota obtida na P1: ";
+    cin >> fNotaP1;
+
+    cout << "Digite a nota obtida na P2: ";
+    cin >> fNotaP2;
+
+    fMed = (fNotaP1+fNotaP2)/2;
+
+    cout << "\n Sua média foi de: " << fMed;
+
+    if (fMed >= 7.0)
+    {
+        cout << "\n>>> Parabéns, você está aprovado\n";
+    }
+    else if (fMed < 7.0 && fMed >= 3.0)
+    {
+        cout << "\n>>> Você está de exame\n";
+    }
+
+    else if (fMed < 3.0)
+    {
+        cout << "\n>>> Você está reprovado\n";
+    }
+
+
+
+    return 0;
+}

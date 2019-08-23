952087afdafa325f190dfa33e1b9663ec4c51f0e
Gabrielcosoares <53536978+Gabrielcosoares@users.noreply.github.com>
2019-08-22 21:38:35 -0300

    Update exercicio5.cpp

--- Atividades/Gabriel Correia/lista 2/exercicio5.cpp
+++ Atividades/Gabriel Correia/lista 2/exercicio5.cpp
@@ -1,8 +1,8 @@
-//5. Faça um programa que receba dois números e execute as operações listadas a seguir, de acordo com a escolha
-//do usuário.
+//5. FaÃ§a um programa que receba dois nÃºmeros e execute as operaÃ§Ãµes listadas a seguir, de acordo com a escolha
+//do usuÃ¡rio.
 //**********************************************************
 //- Autor: Gabriel Correia Soares
-//- Descrição: Lista 2 - Exercício 5
+//- DescriÃ§Ã£o: Lista 2 - ExercÃ­cio 5
 //**********************************************************/
 
 #include <iostream>
@@ -13,41 +13,49 @@ using namespace std;
 
 int main()
 {
-    int inumero1 = 0;
-    int inumero2 = 0;
+    float fnumero1 = 0;
+    float fnumero2 = 0;
     int inumero3 = 0;
 
     setlocale(LC_ALL,"");
     system("color 7E");
 
     cout << "primeiro numero: ";
-    cin  >> inumero1;
+    cin  >> fnumero1;
     cout << endl;
     cout << "segundo numero: ";
-    cin  >> inumero2;
+    cin  >> fnumero2;
     cout << endl;
-    cout << "insira o que voce deseja fazer com estes numeros: "<<endl<<"1-media entre os numero digitados;    "<<"2-diferença do maior pelo menor;"<<endl<<"3-produto dos numeros digitados;      "<<"4-divisão do primeiro pelo segundo"<<endl<<"campo de resposta:";
+    cout << "insira o que voce deseja fazer com estes numeros: "<<endl<<"1-media entre os numero digitados;    "<<"2-diferenÃ§a do maior pelo menor;"<<endl<<"3-produto dos numeros digitados;      "<<"4-divisÃ£o do primeiro pelo segundo"<<endl<<"campo de resposta:";
     cin  >> inumero3;
     cout << endl;
 
     if (inumero3>=1&&inumero3<5)
-        {if (inumero3==1)
-               {cout<<"media dos numeros digitados e:"<<(inumero1+inumero2)/2;}
+        {
+        if (inumero3==1)
+            {
+            cout<<"media dos numeros digitados e:"<<(fnumero1+fnumero2)/2;
+            }
         if  (inumero3==2)
-            {if(inumero1>inumero2)
-            {cout<<inumero1-inumero2;}
+            {
+            if(fnumero1>fnumero2)
+            {cout<<fnumero1-fnumero2;}
             else
-            { cout<<inumero2-inumero1;}}
+            { cout<<fnumero2-fnumero1;}
+            }
         if  (inumero3==3)
-            {cout<<inumero1*inumero2;}
+            {
+            cout<<fnumero1*fnumero2;
+            }
         if (inumero3==4)
-            {if (inumero2>0)
-            {cout<<inumero1/inumero2;}
-            if (inumero2<0)
-            {cout<<inumero1/inumero2;}
-    else
-        cout<<"sinto muito mas nao foi possivel fazer esta operacao";}}
-    else
-        cout<<"operacao innvalida";
+            {
+            if (fnumero2!=0)
+            {cout<<fnumero1/fnumero2;}
+            else
+            cout<<"sinto muito mas nao foi possivel fazer esta operacao";
+            }
+        }
+    else if (inumero3<1||inumero3>=5)
+        cout<<"operacao innvalida(numero colocado fora do menu)";
 
 }

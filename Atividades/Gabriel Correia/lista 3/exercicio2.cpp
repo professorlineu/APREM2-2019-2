b014d1d9cb155fa2513f8ade7124ea2124389592
Gabrielcosoares <53536978+Gabrielcosoares@users.noreply.github.com>
2019-08-27 22:52:31 -0300

    Add files via upload

---
+++ Atividades/Gabriel Correia/lista 3/exercicio2.cpp
@@ -0,0 +1,33 @@
+//**********************************************************
+//- Autor: Gabriel Correia Soares
+//- Descrição: Lista 3 - Exercício 2
+//**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+
+    int i = 0;
+    int iIng = 120;
+    float fPre = 5.00;
+
+    while(i<9)
+    {
+    cout<<"valor do ingresso: R$"<<fPre<<"\n";
+    cout<<"ingressos vendidos:  "<<iIng<<"\n";
+    cout<<"valor lucro total: R$"<<(iIng*fPre)-200<<"\n";
+    iIng=iIng+26;
+    fPre=fPre-0.50;
+    i=1+i;
+
+    system("pause");
+    }
+
+
+
+    }

850162c52fe7df73c618de806395816c4aab4247
Vitor Kenzo Kikuchi <53536885+vitork3nzo@users.noreply.github.com>
2019-09-03 18:17:36 -0300

    EX 25 - Programa Investimentos

---
+++ Atividades/Vitor Kenzo Kikuchi/3.4 Cap.05 - Prog22.cpp
@@ -0,0 +1,79 @@
+/**********************************************************
+- Autor:     Vitor Kenzo Kikuchi
+- Descrição: Exercício 25 - Este programa calcula o rendimento de vários investimentos
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+main()
+{
+    int iOpcao = 0;
+    int i = 1;
+    float fDin = 0;
+    int iCodigo = 0;
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+while (i!=0)
+{
+system("cls");
+cout << "\nAperte 0 para sair ou qualquer outro número para continuar... ";
+cin  >> i;
+
+if (i==0)
+
+{
+return 0;
+}
+
+else if (i>0)
+{
+cout << "\n\n\n     Qual é seu código? ";
+cin  >> iCodigo;
+
+cout << "\n\n     Qual é o valor que você deseja investir? ";
+cin  >> fDin;
+
+cout << "\n\nPara investir na poupança, digite 1.\n"
+     << "\nPara investir na poupança Plus, digite 2.\n"
+     << "\nPara investir em fundos de renda fixa, digite 3.\n\n\n                       ";
+cin  >> iOpcao;
+}
+
+if (iOpcao == 1)
+{
+cout << "\n\nNa poupança, seu dinheiro rende 1.5% por mês.\n\n"
+     << "No final do primeiro mês, você teria "
+     << fDin * 1.5/100 + fDin
+     << " reais.\n\n "
+     << endl;
+}
+else if (iOpcao == 2)
+{
+cout << "\n\nNa poupança Plus, seu dinheiro rende 2% por mês.\n\n"
+     << "No final do primeiro mês, você teria "
+     << fDin * 2/100 + fDin
+     << " reais.\n\n "
+     << endl;
+}
+else
+{
+cout << "\n\nNa Renda Fixa, seu dinheiro rende 4% por mês.\n\n"
+     << "No final do primeiro mês, você teria "
+     << fDin * 4/100 + fDin
+     << " reais.\n\n "
+     << endl;
+}
+
+system("pause");
+}
+
+
+    return 0;
+}

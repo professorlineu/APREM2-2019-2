88f8d3b3db4e8b38678e214e89d64d7c2906e6bb
Isabela Aparecida de Souza <53537161+IsabelaApSouza@users.noreply.github.com>
2019-09-04 17:54:10 -0300

    Ex04 - Programa idade, altura, peso.

--- Atividades/ Isabela/Lista 3/Exercicio04-L3.cpp
+++ Atividades/ Isabela/Lista 3/Exercicio04-L3.cpp
@@ -1,6 +1,6 @@
 /**********************************************************
-- Autor:  Isabela Aparecida de Souza
-- Descrição: EX04 tabuada 5
+- Autor:     Lineu Lima
+- Descrição: Comando while
 **********************************************************/
 #include <iostream>
 #include <locale.h>
@@ -10,18 +10,27 @@ using namespace std;
 
 int main()
 {
+    //Declaração de variávei
     int i = 0;
-    int iValor = 0;
+    float fIdade = 0;
+    float fAltura = 0;
+    float fPeso = 0;
 
+    //Configuração da tela de saída
     setlocale(LC_ALL,"");
     system("color F3");
 
-    cout << "Digite um número: ";
-    cin >> iValor;
-    while (i < 11)
-    {
-        cout << iValor << " x " << i << " = " << iValor*i << endl;
-        i = i + 1;
-    }
-    return 0;
-}
+    //Código do programa
+
+    while (i < 2)
+ {
+        cout << "Digite sua idade: " ;
+        cin >> fIdade;
+
+        cout << "Digite sua altura: " ;
+        cin >> fAltura;
+
+        cout << "Digite sua Peso: " ;
+        cin >> fPeso;
+        i++;
+ }

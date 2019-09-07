2de2b136e83ccdd2b3335eaf769f5c11420b5999
Isabela Aparecida de Souza <53537161+IsabelaApSouza@users.noreply.github.com>
2019-09-07 11:13:05 -0300

    EX16 - Programa media de idades.

---
+++ Atividades/ Isabela/Lista 3/Exercicio16-L3.cpp
@@ -0,0 +1,30 @@
+//************************************
+//Nome: Isabela Aparecida de Souza
+//Descrição: Media das idades
+//************************************
+
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+int i= 0;
+int iIdade = 0;
+int iIdd = 0;
+
+    //setlocale(LC_ALL,"");
+    //system("color F3");
+
+while (i < 10)
+{
+  cout << "Digite sua idade:" ;
+  cin >> iIdade;
+
+  iIdd=iIdd+iIdade;
+  i++;
+  } 
+  cout << "A média das idades: " << iIdd/10 ;
+}

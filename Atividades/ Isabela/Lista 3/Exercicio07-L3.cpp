0a187e6f5a210ec7dd22d0662c83001417a8484e
Isabela Aparecida de Souza <53537161+IsabelaApSouza@users.noreply.github.com>
2019-09-07 10:07:31 -0300

    Update Exercicio07-L3.cpp

--- Atividades/ Isabela/Lista 3/Exercicio07-L3.cpp
+++ Atividades/ Isabela/Lista 3/Exercicio07-L3.cpp
@@ -1,6 +1,6 @@
 /**********************************************************
-- Autor:     Lineu Lima
-- Descrição: Comando while
+- Autor: Isabela Aparecida de Souza
+- Descrição: Idade, altura e peso
 **********************************************************/
 #include <iostream>
 #include <locale.h>
@@ -10,27 +10,56 @@ using namespace std;
 
 int main()
 {
-    //Declaração de variávei
     int i = 0;
+    int ii = 0;
     float fIdade = 0;
     float fAltura = 0;
     float fPeso = 0;
+    float ftotal = 0;
+    int insei = 0;
+    int inaosei2 = 0;
 
-    //Configuração da tela de saída
-    setlocale(LC_ALL,"");
-    system("color F3");
+    //setlocale(LC_ALL,"");
+    //system("color F3");
 
-    //Código do programa
-
-    while (i < 2)
- {
-        cout << "Digite sua idade: " ;
+    while (i < 3)
+    {
+        cout << "\nDigite sua idade: ";
         cin >> fIdade;
 
-        cout << "Digite sua altura: " ;
+        cout << "Digite sua altura: ";
         cin >> fAltura;
 
-        cout << "Digite sua Peso: " ;
+        cout << "Digite seu Peso: ";
         cin >> fPeso;
         i++;
- }
+
+        if (fIdade > 50)
+        { 
+          ii++;
+        }
+	
+        if (fIdade> 10 && fIdade < 20 )
+        {
+          ftotal=ftotal+fIdade;
+         
+          insei++;
+        }
+
+        if (fPeso < 40)
+       { 
+         inaosei2++;
+
+	     }
+    }
+        cout << "O número de pessoas com idade superior a 50 anos é:" <<ii <<endl;
+       if (insei != 0)
+        {
+          cout << "A media de pessoas entre 10 e 20 anos é:" <<ftotal/insei <<endl;
+        }
+        if (insei == 0)
+        {
+          cout << "A media de pessoas entre 10 e 20 anos é: 0";
+        }
+        cout << "A porcentagem de pessoa com peso inferior a 40kg: " << (inaosei2*100)/3;
+}

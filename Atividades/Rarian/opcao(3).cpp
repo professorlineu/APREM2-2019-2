ecd02ea6b483acdb48249c84bada7486746c3611
RarianSetoguchi <53540013+RarianSetoguchi@users.noreply.github.com>
2019-09-11 17:56:25 -0300

    escolha uma opção

--- Atividades/Rarian/opcao(3).cpp
+++ Atividades/Rarian/opcao(3).cpp
@@ -14,8 +14,6 @@ int main()
     //Declaração de variáveis
     int i = 0;
     int d = 0;
-    int F = 0;
-    int f = 0;
     int iopcao = 0;
     float fnota = 0;
     float fnota2 = 0;
@@ -31,62 +29,65 @@ int main()
     //Código do programa
     while (d !=1)
     {
-    cout << "Escolha uma das opções a seguir:\n";
-    cout << "/********************************************************** \n";
-    cout << "1. Media aritmética \n";
-    cout << "2. Media ponderada \n";
-    cout << "3. Sair \n";
-    cout << "**********************************************************/ \n";
-    cout << "Digite a opção escolhida: ";
-    cin >> iopcao;
+        cout << "Escolha uma das opções a seguir:\n";
+        cout << "/********************************************************** \n";
+        cout << "1. Media aritmética \n";
+        cout << "2. Media ponderada \n";
+        cout << "3. Sair \n";
+        cout << "**********************************************************/ \n";
+        cout << "Digite a opção escolhida: ";
+        cin >> iopcao;
 
 
 
-    switch (iopcao)
-    {
+        switch (iopcao)
+        {
         case 1:
-            while (d !=1)
+            while (fnota !=-1)
             {
                 system("cls");
-                cout << "Você escolheu a opção de Media aritmética. Para finalizar digite F ou f. \n";
+                cout << "Você escolheu a opção de Media aritmética. Para finalizar digite -1. \n";
                 cout << "Digite as notas: ";
                 cin >> fnota;
+                if (fnota !=-1)
+                {
                 fsoma = fsoma+fnota;
                 i++;
+                }
+                if (fnota == -1)
+                {
+                    system("cls");
+                    --i;
+                    fmedia = fsoma/i;
+                    cout << " A sua media é " << fmedia << ".\n";
+                }
 
-                if (fnota == f || fnota == F)
-        {
-                system("cls");
-                --i;
-                fmedia = fsoma/i;
-                cout << " A sua media é " << fmedia << ".\n";
-                d++;
-
-        } // fim do if 1
             } // fim do while 1
             break;
 
         case 2:
-            while (d != 1)
+            while ( fnota2!= -1)
             {
                 system("cls");
-                cout << "Você escolheu a opção de Media ponderada. Para finalizar digite F ou f. \n";
+                cout << "Você escolheu a opção de Media ponderada. Para finalizar digite -1. \n";
                 cout << "Digite as notas: ";
                 cin >> fnota2;
-                cout << "Digite o peso desta nota: ";
+                if (fnota2!= -1)
+                {cout << "Digite o peso desta nota: ";
                 cin >> fpeso;
                 fsoma = fsoma+(fnota2*fpeso);
                 fsomapeso = fsomapeso + fpeso;
+                }
+                 if (fnota2 == -1)
+                {
+                    system("cls");
+                    fmedia = fsoma/fsomapeso;
+                    cout << " A sua media é " << fmedia << ".\n";
 
-                 if (fnota2 == f || fnota2 == F)
-        {
-                system("cls");
-                fmedia = fsoma/fsomapeso;
-                cout << " A sua media é " << fmedia << ".\n";
-                d++;
+                }
+            }
 
-        } // fim do if 2
-            } // fim do while 2
+             // fim do while 2
             break;
 
         case 3:
@@ -101,7 +102,7 @@ int main()
             system("cls");
             break;
 
-    } // fim do switch
+        } // fim do switch
 
     } // fim do while
 

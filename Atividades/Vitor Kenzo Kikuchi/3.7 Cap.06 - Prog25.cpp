2be27a0482653fd6e47b4efa9b518a39da595178
Vitor Kenzo Kikuchi <53536885+vitork3nzo@users.noreply.github.com>
2019-09-11 15:58:13 -0300

    .

---
+++ Atividades/Vitor Kenzo Kikuchi/3.7 Cap.06 - Prog25.cpp
@@ -0,0 +1,80 @@
+/**********************************************************
+- Autor:      Lineu Lima
+- Descrição:  Vetor
+**********************************************************/
+
+#include <iostream>
+#include <cstdlib>
+#include <locale>
+
+using namespace std;
+
+int main()
+{
+    int i = 0;
+
+    int vet[6];
+
+    int iContPar = 0;
+
+    int iContImpar = 0;
+
+
+    setlocale (LC_ALL,"");
+    system ("color F1");
+
+    for (i = 0; i < 6; i++)
+    {
+        cout << "Valor " << i + 1 << ": ";
+        cin >> vet[i];
+    }
+
+    system("cls");
+    cout << "\n\n\tNúmeros Pares: \n" << endl;
+
+
+    for (i = 0; i < 6; i++)
+    {
+
+    if (vet[i] % 2 == 0)
+    {
+    cout << vet[i] << "\t";
+
+    iContPar += 1;
+    }
+
+    }
+
+    cout << "\n\nQuantidade de números pares: "
+         << iContPar
+         << endl;
+
+
+    cout << "\n\n\tNúmeros impares: \n" << endl;
+
+    for (i = 0; i < 6; i++)
+    {
+
+    if (vet[i] % 2 != 0)
+    {
+    cout << vet[i] << "\t";
+
+    iContImpar += 1;
+    }
+
+    }
+
+    cout << "\n\nQuantidade de números impares: "
+         << iContImpar
+         << endl;
+
+
+
+
+
+
+    return 0;
+}
+
+
+

8f7149ffdba93364ef4ccd1c2839cd2bc531ff77
Thomas Nicolas de Lana Taquetto <53537358+ThomasTaquetto@users.noreply.github.com>
2019-09-11 16:04:41 -0300

    upload de exercicios

--- Atividades/Thomas_Nicolas/Lista 03/Lista 03 ex 04.cpp
+++ Atividades/Thomas_Nicolas/Lista 03/Lista 03 ex 04.cpp
@@ -1,35 +1,34 @@
 /**********************************************************
-- Autor:
+- Autor:     Thomas Nicolas
 - Descrição:
 **********************************************************/
+
 #include <iostream>
 #include <locale.h>
 #include <cstdlib>
 
 using namespace std;
 
-int main()
+main()
 {
-    //Declaração de variáveis
+    //Declaração das variáveis
 
     int i = 0;
-    int inum = 0;
+    float fvalor = 0;
 
     //Configuração da tela de saída
+
     setlocale(LC_ALL,"");
     system("color F1");
 
-    //Código do programa
+    //código do programa
 
-    cout    <<"Digite um número:";
-    cin     >>inum;
+    cout << "Digite um número: ";
+    cin >> fvalor;
 
-    while   (i<11)
+    while (i<11)
     {
-        cout <<inum <<" X " <<i <<" = " <<inum*i;
-        cout << "\n";
-            i+=1;
+        cout << fvalor <<" X " << i <<" = " <<i*fvalor <<"\n";
+        i++;
     }
-
-    return 0;
 }

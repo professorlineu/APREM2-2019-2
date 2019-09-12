dbb8a6f9146625283970f880d6681f2cc799db6a
Vanderlei Natanael Bezerra Gouvêa <53536992+naelgouveaa@users.noreply.github.com>
2019-09-11 21:17:11 -0300

    Create Atividade 4.cpp

---
+++ Vanderlei/Lista 3/Atividade 4.cpp
@@ -0,0 +1,43 @@
+/**********************************************************
+- Autor:     Vanderlei Natanael
+- Descrição: Lista 3 - Ex 5
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+
+    int i = 0;
+    int iNum = 1;
+    int iResult = 0;
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+
+    while (iNum < 11) 
+    {
+
+        while (i < 11) 
+       {
+        iResult = (iNum) * i;
+
+       cout << iNum << " X " << i << " = " << iResult << endl;
+
+        i++;
+
+       }
+       cout << endl << endl;
+        iNum = iNum + 1;
+        i = 0;
+    }
+
+    return 0;
+}

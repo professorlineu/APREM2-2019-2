208942e3e731a5b9bff719e1b54b019782f8347b
Vitor Kenzo Kikuchi <53536885+vitork3nzo@users.noreply.github.com>
2019-09-12 13:47:43 -0300

    .

---
+++ Atividades/Vitor Kenzo Kikuchi/3.8 Cap.06 - Prog26.cpp
@@ -0,0 +1,52 @@
+/**********************************************************
+- Autor:      Vitor Kenzo Kikuchi
+- Descrição:  Exercício
+**********************************************************/
+
+#include <iostream>
+#include <cstdlib>
+#include <locale>
+
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    int inum[10];
+
+
+    setlocale(LC_ALL,"");
+    system ("Color F1");
+
+    for (i=0; i<10; i+=1)
+    {
+    cout << "\nDigite o " << i+1 << "° número: ";
+    cin  >> inum[i];
+    }
+
+    system("cls");
+
+    cout << "\n\n\t\tNúmeros pares: \n" << endl;
+
+    for (i=0; i<10; i++)
+    {
+        if (inum[i]%2 == 0)
+        cout << inum[i] << "\t";
+    }
+
+    cout << "\n\n\t\tNúmeros impares: \n" << endl;
+
+    for (i=0; i<10; i++)
+    {
+        if (inum[i]%2 != 0)
+        cout << inum[i] << "\t";
+    }
+
+    cout << "\n\n" << endl;
+
+    return 0;
+
+}
+
+
+

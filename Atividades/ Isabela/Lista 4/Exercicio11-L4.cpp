01378591766b3334347f275a2436f316bd666315
Isabela Aparecida de Souza <53537161+IsabelaApSouza@users.noreply.github.com>
2019-09-18 17:04:29 -0300

    EX11 - Programa impar, par.

---
+++ Atividades/ Isabela/Lista 4/Exercicio11-L4.cpp
@@ -0,0 +1,42 @@
+    //**********************************************************
+    //Nome: Isabela ap de souza
+    //Descrição: Exercicio 11 -  L4
+    //**********************************************************
+
+    #include <iostream>
+    #include <cstdlib>
+    #include <locale>
+
+    using namespace std;
+
+    int main()
+    {
+    int i = 0;
+    int iNum[10];
+
+    setlocale(LC_ALL,"");
+    system ("Color F8");
+
+    for (i=0; i<10; i+=1)
+    {
+    cout << "Digite o " << i+1 << "° número: ";
+    cin  >> iNum[i];
+    }
+    cout << "\n\t\tNúmeros pares: \n" << endl;
+    for (i=0; i<10; i++)
+    {
+        if (iNum[i]%2 == 0)
+        cout << iNum[i] << "\t";
+    }
+
+    cout << "\n\t\tNúmeros impares: \n" << endl;
+    for (i=0; i<10; i++)
+    {
+        if (iNum[i]%2 != 0)
+        cout << iNum[i] << "\t";
+    }
+
+    cout << "\n\n" << endl;
+
+    return 0;
+}

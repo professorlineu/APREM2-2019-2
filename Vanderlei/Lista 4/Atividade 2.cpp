2fbc66c6508d0c5d77db941e3ef83a4d1e1e0e1a
Vanderlei Natanael Bezerra Gouvêa <53536992+naelgouveaa@users.noreply.github.com>
2019-09-18 17:46:08 -0300

    Create Atividade 2.cpp

---
+++ Vanderlei/Lista 4/Atividade 2.cpp
@@ -0,0 +1,82 @@
+/*********************************************************
+ -Autor:      Vanderlei Natanael
+ -Descrição:  Vetor
+**********************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 1;
+
+    int vet[7];
+
+    int iContD = 0;
+
+    int iContT = 0;
+
+    int iValorDT =0;
+
+
+    for (i = 1; i < 8; i++)
+    {
+        cout << "Valor " << i << ": ";
+        cin >> vet[i];
+    }
+
+
+    cout << "\nElementos do vetor: " << endl;
+
+    for (i = 1; i < 8; i++)
+    {
+        cout << vet[i] << "\t";
+
+        if (vet[i] % 2 == 0)
+        {
+            iContD++;
+        }
+
+        else
+        {
+            iContT++;
+        }
+
+    }
+
+
+    cout << "\n\nMultiplos de 2: " << endl;
+
+        for (i = 1; i < 8; i++)
+        {
+        if (vet[i] % 2 == 0)
+        {
+            cout << vet[i] << " ";
+        }
+        }
+
+    cout << "\n\nMultiplos de 3: " << endl;
+
+     for (i = 1; i < 8; i++)
+        {
+        if (vet[i] % 3 == 0)
+        {
+            cout << vet[i] << " ";
+        }
+
+        }
+
+    cout << "\n\nMultiplos de 2 e 3: " << endl;
+
+    for (i = 1; i < 8; i++)
+        {
+        if (vet[i] % 2 == 0 & vet[i] % 3 == 0)
+        {
+            cout << vet[i] << " ";
+        }
+
+        }
+
+    return 0;
+}

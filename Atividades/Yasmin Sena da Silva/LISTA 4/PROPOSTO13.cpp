27389f0cb1d53dca47d96e28ef449007ffa410c5
Yasmin Sena da Silva <53537149+yasminsena@users.noreply.github.com>
2019-09-18 15:54:04 -0300

    corrigir

---
+++ Atividades/Yasmin Sena da Silva/LISTA 4/PROPOSTO13.cpp
@@ -0,0 +1,47 @@
+/**********************************************************
+- Autor:      YASMIN SENA
+- Descrição:  PROPOSTO 13
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+#include <ctype.h>
+
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    float vetnota[8];
+    string vetnome[8];
+    float fmedia = 0;
+    float fsoma=0;
+   //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    // entrada de dados
+    for (i = 0; i < 8; i++)
+    {
+        cout <<"Digite o nome do "<< i+1 << " º Aluno: ";
+        cin >> vetnome[i];
+        cout << "\n Digite a nota do "<< vetnome[i] <<" : ";
+        cin >> vetnota[i];
+    }
+
+    cout << "\n Relatórios de notas: \n" << endl;
+    // imprime o vetor
+    for (i = 0; i < 8; i++)
+    {
+        cout << vetnome[i] << " " << vetnota[i]<<"\n";
+        fsoma= fsoma+ vetnota[i];
+    }
+
+    fmedia = fsoma/8;
+    cout << "\n Média da classe = \n"<< fmedia;
+
+    return 0;
+}
+
+
+

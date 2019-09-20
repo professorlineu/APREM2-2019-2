17e19f24dc00c4c61f4c36c1efebcdd1914a2d47
RarianSetoguchi <53540013+RarianSetoguchi@users.noreply.github.com>
2019-09-20 14:45:09 -0300

    L4 Ex21

---
+++ Atividades/Rarian/Lista 4/Ex 21.cpp
@@ -0,0 +1,56 @@
+/**********************************************************
+- Autor:     Rarian
+- Descrição: L4 Ex21
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+
+    int i = 0;
+    float vet[10];
+
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+
+
+    for (i = 0; i < 10; i++)
+    {
+        cout << "Digite qualquer número para o valor "<< i + 1 << ": ";
+        cin >> vet[i];
+        system("cls");
+    }
+
+
+    for (i = 0; i < 10; i++)
+    {
+
+        cout << vet[i] << "\t";
+    }
+
+    cout << "\n\n";
+
+    for (i = 0; i < 10; i++)
+    {
+
+        if (vet[i] == 0)
+        {
+            cout  << vet[i] +1 << "\t";
+        }
+        else
+        {
+            cout << vet[i] << "\t";
+        }
+    }
+
+    return 0;
+}

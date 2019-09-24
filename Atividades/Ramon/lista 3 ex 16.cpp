6879f3e32371dc7779a0c42924b689353e41024d
Ramon Oliveira Macedo <53537120+ramonmacedo98@users.noreply.github.com>
2019-09-24 10:22:19 -0300

    lista 3 exercicio 16

---
+++ Atividades/Ramon/lista 3 ex 16.cpp
@@ -0,0 +1,42 @@
+/**********************************************************
+- Autor:     Ramon Macedo
+- Descrição:
+**********************************************************/
+
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+
+    int i = 0;
+    float idade = 1;
+    float itotal = 0;
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+
+    while (idade != 0)
+    {
+        idade = 0;
+        cout << "Digite a idade: ";
+        cin >> idade;
+
+            if (idade != 0)
+               {
+                i+=1;
+                itotal = idade+itotal;
+               }
+    } //fim do while
+
+        cout << "A média das idades é: " << (itotal)/i;
+
+    return 0;
+} //fim do programa

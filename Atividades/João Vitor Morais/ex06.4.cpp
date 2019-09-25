5cca7528329a9a9c37963e79efc701dd23647048
Joaovitormorais <53537236+Joaovitormorais@users.noreply.github.com>
2019-09-25 09:58:33 -0300

    vendas e comissão

--- Atividades/João Vitor Morais/ex06.4.cpp
+++ Atividades/João Vitor Morais/ex06.4.cpp
@@ -6,8 +6,6 @@ Receba os nomes desses vendedores e armazene-os em um terceiro vetor. Existem ap
 loja. Calcule e mostre:
 ■■ um relatório com os nomes dos vendedores e os valores a receber referentes à comissão;
 ■■ o total das vendas de todos os vendedores;
-■■ o maior valor a receber e o nome de quem o receberá;
-■■ o menor valor a receber e o nome de quem o receberá.
 **********************************************************/
 #include <iostream>
 #include <locale.h>
@@ -22,7 +20,7 @@ int main()
 string snome[10];
 float fvendas[10];
 float fcomis[10];
-float total = 0;
+float ftotal = 0;
 int i = 0;
 
     //Configuração da tela de saída
@@ -49,7 +47,17 @@ for (i=0; i<10; i++)
     cin  >> fcomis[i];
 }
 
+for (i=0; i<10; i++)
+{
+    cout << snome[i] << "tem comissão de: " << fvendas[i]*(fcomis[i]/100.0);
+}
 
+for (i=0; i<10; i++)
+{
+   ftotal = ftotal + fvendas[i];
+
+ cout << "Somando todas as vendas, temos o valor total de: " << ftotal << " reais.";
+}
 
     return 0;
 }
